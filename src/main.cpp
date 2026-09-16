ericshantos/line-follower

feat: add the analog sensor

src/main.cpp
@@ -1,13 +1,13 @@
 #include "pico/stdlib.h"
 #include "Engine.hpp"
-#include "Sensor.hpp"
+#include "AnalogSensor.hpp"
 #include "Pins.hpp"
 
 
 int main() {
 
-    Sensor sensor_left(Pins::Sensor::LEFT);
-    Sensor sensor_right(Pins::Sensor::RIGHT);
+    AnalogSensor sensor_left(Pins::Sensor::LEFT, 0);
+    AnalogSensor sensor_right(Pins::Sensor::RIGHT, 1);
 
     Engine left(
         Pins::Engine::LEFT_1,
@@ -21,8 +21,8 @@ int main() {
 
     while (true) {
         
-        bool sR = sensor_left.read();
-        bool sL = sensor_right.read();
+        float sR = sensor_left.read_normalized();
+        float sL = sensor_right.read_normalized();
 
         if (sL && sR) {
             left.stop();

ericshantos/rastro

src/main.cpp
@@ -1,13 +1,13 @@
 #include "pico/stdlib.h"
 #include "Engine.hpp"
-#include "Sensor.hpp"
+#include "AnalogSensor.hpp"
 #include "Pins.hpp"
 
 
 int main() {
 
-    Sensor sensor_left(Pins::Sensor::LEFT);
-    Sensor sensor_right(Pins::Sensor::RIGHT);
+    AnalogSensor sensor_left(Pins::Analog::LEFT, 0);
+    AnalogSensor sensor_right(Pins::Analog::RIGHT, 1);
 
     Engine left(
         Pins::Engine::LEFT_1,
@@ -21,8 +21,8 @@ int main() {
 
     while (true) {
         
-        bool sR = sensor_left.read();
-        bool sL = sensor_right.read();
+        float sR = sensor_left.read_normalized();
+        float sL = sensor_right.read_normalized();
 
         if (sL && sR) {
             left.stop();